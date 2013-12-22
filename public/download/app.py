b5cd2d038b89a99876dd26ba05b3a676e4bca44f
James Bailey <jimi.bailey@gmail.com>
2013-12-22 11:53:41 +0000

    Updated the python app

--- public/download/app.py
+++ public/download/app.py
@@ -6,9 +6,14 @@ class Namespace(BaseNamespace):
         
     def on_update(self, *args):
         data = args[0]
-        print data["device"], data["controller"], data["value"]
+        print data["index"], data["controller"], data["value"]
 
 socketIO = SocketIO('xbox.jimib.co.uk', 80, Namespace)
 
 while True:
-    socketIO.wait(seconds=1)
+    try:
+        socketIO.wait(seconds=1)
+    except KeyboardInterrupt:
+        break
+    except:
+        print "Error occured - ignoring"
