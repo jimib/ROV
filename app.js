98932726134fdfb1af1e36218c6233d63fcc4f0c
James Bailey <jimi.bailey@gmail.com>
2013-12-22 00:24:16 +0000

    Added swf file and the connecting code

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express = require("express"),
 	path = require("path");
 	
 var app = express();
-require("./lib/sockets")(app);
+var io = require("./lib/sockets")(app);
 
 var config = require("./config.json");
 
@@ -12,6 +12,26 @@ app.configure(function(){
 	app.use(require("./app/home"));
 	app.use(app.router);
 	app.use(express.static(path.join(__dirname, "public")));
+});
+
+io.of("controller").on("connection", function(){
+	console.log("controller connected");
+	
+	socket.on("added", function(data){
+		io.of("client").emit("added", data);
+	});
+	
+	socket.on("removed", function(data){
+		io.of("client").emit("removed", data);
+	});
+	
+	socket.on("update", function(data){
+		io.of("client").emit("update", data);
+	});
+});
+
+io.of("client").on("connection", function(){
+	console.log("client connected");
 });
 
 app.listen(config.port);
